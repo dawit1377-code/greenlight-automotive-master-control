1173d882037bda90a66065cfa5be6bee0bf87ee4
dawit1377-code <dawit1377@gmail.com>
2026-08-14 06:24:24 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist'
-  }
-});
+  base: './', // Ensures relative asset loading for Electron and GitHub Pages
+})
